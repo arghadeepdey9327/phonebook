285a6310bd10a5f892f62e37ed722855c119a013
arghadeepdey9327 <65493161+arghadeepdey9327@users.noreply.github.com>
2022-07-14 21:23:34 +0530

    2.getting_data_from_server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
-  )
+import axios from 'axios'
+axios.get('http://localhost:3001/persons').then(a=>ReactDOM.createRoot(document.getElementById('root')).render(
+  <App axiosData={a.data}/>
+))
+  
